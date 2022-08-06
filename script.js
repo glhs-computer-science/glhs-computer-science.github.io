75bac2338e278df3fe0cf287598f9b0cc2365e42
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2022-08-05 23:27:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//SMOOTH SCROLLING
+const links = document.querySelectorAll(".page-header ul a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
+// MOBILE BUTTON NAVIGATION THINGY
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
